aaed97fec87ed02d27c35653ffc0311dc804a65f
VaishnaviBaliga <30418328+VaishnaviBaliga@users.noreply.github.com>
2017-08-17 20:29:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,7 @@ element.innerHTML = 'new value';
 
 //move image
 var img = document.getElementById('madi');
-img.onclick = function (){
-    img.style.marginleft = '100px';
+img.onclick = function ()
+{
+    img.style.marginLeft = '100px';
 };
